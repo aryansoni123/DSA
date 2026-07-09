f6f13567dfd45e587d0e9b8d6e542013ea663de4
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-07-10 03:14:13 +0530

    Time: 371 ms (25.89%) | Memory: 14.6 MB (38.29%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -29,9 +29,31 @@ public:
 
         // for() max1 = max(max1, it);
 
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        // vector<vector<int>> dp(n, vector<int>(n+1, -1));
 
-        return f(0, -1, nums, dp);
+        vector<int> front(n+1, 0);
+        vector<int> curr(n+1, 0);
+        int ans = INT_MIN;
+
+        for(int i =  n-1; i>=0; i--){
+            for(int j = n-1; j>=-1; j--){
+                
+                int pick = 0, not_pick = 0;
+
+                if(j == -1 || nums[j] < nums[i]) pick = 1 + front[i+1];
+                not_pick = front[j+1];
+                
+                curr[j+1] = max(pick, not_pick);
+                ans = max(ans, curr[j+1]);
+            }
+            swap(curr, front);
+        }
+
+        return ans;
+
+
+
+        // return f(0, -1, nums, dp);
         // return f(0, INT_MIN, nums);
         
     }
