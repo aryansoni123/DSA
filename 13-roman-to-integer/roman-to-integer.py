6b8c947fab8ee7b111e183514f97f1d375a5a3ef
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-07-12 03:02:46 +0530

    Time: 11 ms (14.49%) | Memory: 19.2 MB (88.76%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -15,17 +15,12 @@ class Solution:
         i = n-2
         m = s[-1]
         ans = val[m]
-
-        # print(ans, m)
-
+        
         while i>=0:
-            # print(i)
             if val[s[i]]<val[m]:
-                # print('a', s[i])
                 ans-=val[s[i]]
 
             elif val[s[i]]>=val[m]:
-                # print('b', s[i])
                 ans+=val[s[i]]
                 m = s[i]
 
