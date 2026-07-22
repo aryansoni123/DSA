593ffdb8d01f83b55134dc4a783529bad0547f42
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-07-22 14:48:03 +0530

    Time: 80 ms (46.43%) | Memory: 54.9 MB (58.04%) - LeetSync

--- 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
@@ -7,31 +7,39 @@ public:
 
         for(char c: s) if(c == '1') ini++;
 
-        int i = 0, cnt = 0;
+        int i = 0, cnt = 0, prev = 0;
+        int mx = 0;
 
         while (i<n){
             if (s[i] == '0') cnt++;
 
             else if(s[i] == '1' && cnt!= 0){
-                zeros.push_back(cnt);
+                if (prev == 0){
+                    prev = cnt; 
+                    cnt = 0;
+                    continue;
+                }
+                mx = max(prev+cnt, mx);
+                prev = cnt;
                 cnt = 0;
+                // cnt = 0
+                // zeros.push_back(cnt);
             }
             i++;
         }
 
-        if (cnt!=0) zeros.push_back(cnt);
+        if(prev!=0 && cnt!=0) mx = max(prev+cnt, mx);
+        // if (cnt!=0) zeros.push_back(cnt);
         // cout<<zeros.size();
 
-        if(zeros.size() == 1) return ini;
-        int mx = 0;
-
+        // if(zeros.size() == 1) return ini;
 
         // for(int i: zeros) cout<<i;
 
-        for(int i = 1; i<zeros.size(); i++){
-            mx = max(zeros[i-1]+zeros[i], mx);
-        }
-
+        // for(int i = 1; i<zeros.size(); i++){
+        //     mx = max(zeros[i-1]+zeros[i], mx);
+        // }
+        cout<<ini;
         return mx+ini;
     }
 };
