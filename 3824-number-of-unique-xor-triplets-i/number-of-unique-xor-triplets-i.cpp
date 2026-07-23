f94d6ebf1b494f54ea01fe3465d0d593d4c71166
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-07-23 14:27:31 +0530

    Time: 0 ms (100.00%) | Memory: 199.1 MB (9.38%) - LeetSync

--- 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
@@ -4,19 +4,15 @@ public:
         set<int> s;
 
         int n = nums.size();
-        // int val = nums[0];
         int m;
 
         if (n == 1) return 1;
         else if (n==2) return 2;
 
-        // for(auto x:nums) s.insert(x);
-
         for(int i = 1; i<=17; i++){
             if (n<=pow(2,i) && n>=pow(2,i-1)) m = i;
         }
 
-        cout<<m;
         long long a = pow(2, m);
         return a;
     }
