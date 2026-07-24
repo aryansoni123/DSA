830b8b8f59189f57010aabc7593b04289c3d2683
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-07-24 13:13:42 +0530

    Time: 725 ms (60.67%) | Memory: 35.9 MB (74.72%) - LeetSync

---
+++ 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        set<int> s;
+        set<int> ans;
+
+        int n = nums.size();
+        int mx = 0, rng = 1; 
+
+        for (auto x: nums){
+            mx = max(x, mx);
+        }
+
+        int limit = 1;
+        while (limit <= mx)
+            limit <<= 1;
+
+        for(int i = 1; i<=11; i++){
+            if(pow(2,i-1)<= mx && pow(2, i)>mx){
+                rng = pow(2,i);
+                break;
+            }
+        }
+        // rng = limit;
+        // cout<<rng<<limit<<mx;
+
+        vector<bool> xor_vec(rng+1, false);
+        vector<bool> xor_vec2(rng+1, false);
+
+
+        for(auto x: nums){
+            for(auto y: nums){
+                xor_vec[x^y] = true;
+            }
+        }
+
+        for (int i = 0; i<=rng; i++){
+            if (xor_vec[i]){
+                for(auto x: nums){
+                    xor_vec2[x^i] = true;
+                }
+            }
+        }
+
+        int cnt = 0;
+
+        for(auto x: xor_vec2){
+            if (x) cnt++;
+        }
+
+        // cout<<rng;
+
+        return cnt;
+    }
+};
