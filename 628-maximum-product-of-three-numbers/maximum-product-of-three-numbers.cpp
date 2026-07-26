459cac39b8e58bed6eb29bf8b84e5c34841df25e
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-07-26 17:04:57 +0530

    Time: 19 ms (13.92%) | Memory: 31.6 MB (46.18%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -3,13 +3,62 @@ public:
     int maximumProduct(vector<int>& nums) {
         int n = nums.size();
 
-        sort(nums.begin(), nums.end());
+        priority_queue<int> pqs;
+        priority_queue<int> pql;
 
-        // for(auto x: nums) cout<<x<<' ';
+        int mx = INT_MIN;
 
-        int a = nums[0] * nums[1] * nums[n-1];
-        int b = nums[n-1] * nums[n-2] * nums[n-3];
+        for(int i = 0; i<3; i++){
+            mx = max(nums[i], mx);
+            pqs.push(-nums[i]);
+            pql.push(nums[i]);
+        }
 
-        return max(a,b);
+        for(int i = 3; i<n; i++){
+            mx = max(nums[i], mx);
+            pqs.push(-nums[i]);
+            pql.push(nums[i]);
+            pqs.pop();
+            pql.pop();
+        }
+
+        if (pql.size()>=3) pql.pop();
+
+
+        // sort(nums.begin(), nums.end());
+
+        // while(pqs.size()){
+        //     cout<<-pqs.top();
+        //     pqs.pop();
+        // }
+
+        // cout<<endl;
+
+        // while(pql.size()){
+        //     cout<<pql.top();
+        //     pql.pop();
+        // }
+        // cout<<mx;
+
+        int a = 1;
+
+        while (pqs.size()){
+            a *= -pqs.top();
+            pqs.pop();
+        }
+
+        while(pql.size()){
+            mx *= pql.top();
+            pql.pop();
+        }
+
+        // for(auto x: pqs) cout<<x<<' '<<endl;
+        // for(auto x: pql) cout<<x<<' ';
+
+        // int a = nums[0] * nums[1] * nums[n-1];
+        // int b = nums[n-1] * nums[n-2] * nums[n-3];
+
+        return max(a,mx);
+        // return max(a,b);
     }
 };
