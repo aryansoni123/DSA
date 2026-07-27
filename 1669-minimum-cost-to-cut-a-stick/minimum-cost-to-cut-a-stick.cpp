3bf3c4e5ad3a6657f6ae5de685a9aab736a1904d
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-07-28 00:24:22 +0530

    Time: 43 ms (8.32%) | Memory: 13.9 MB (54.24%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -4,14 +4,16 @@ public:
     int f(int i, int j, vector<int> &arr, vector<vector<int>> &dp){
         if (i>j) return 0;
 
-        int mini = INT_MAX, cuts = 0, cut;
+        int mini = INT_MAX, cuts = 0;
+        int cut = arr[j+1] - arr[i-1];
 
         if (dp[i][j] != -1) return dp[i][j];
 
-        for(int k = i; k<=j; k++){
-            if (i == 0) cut = arr[j+1];
-            else cut = arr[j+1] - arr[i-1];
+        // if (i == 0) cut = arr[j+1];
+        // else 
+        
 
+        for(int k = i; k<=j; k++){
             cuts = cut + f(i, k-1, arr, dp) + f(k+1, j, arr, dp);
             mini = min(cuts, mini);
         }
@@ -26,30 +28,30 @@ public:
         cuts.push_back(0);
         int m = cuts.size();
 
-        // vector<vector<int>> dp(m, vector<int>(m,-1));
-        vector<vector<int>> dp(m, vector<int>(m,0));
+        vector<vector<int>> dp(m, vector<int>(m,-1));
+        // vector<vector<int>> dp(m, vector<int>(m,0));
         sort(cuts.begin(), cuts.end());
 
-        // return f(0, m-2, cuts, dp);
+        return f(1, m-2, cuts, dp);
 
-        int cut, cutss, mini = 0;
+        // int cut, cutss, mini = 0;
 
-        for(int i = m-2; i>0; i--){
-            for(int j = i; j<=m-2; j++){
-                mini = INT_MAX;
-                for(int k = i; k<=j; k++){
-                    // if (i == 0) cut = cuts[j+1];
-                    // else 
-                    cut = cuts[j+1] - cuts[i-1];
+        // for(int i = m-2; i>0; i--){
+        //     for(int j = i; j<=m-2; j++){
+        //         mini = INT_MAX;
+        //         for(int k = i; k<=j; k++){
+        //             // if (i == 0) cut = cuts[j+1];
+        //             // else 
+        //             cut = cuts[j+1] - cuts[i-1];
 
-                    cutss = cut + ((k==i) ? 0 : dp[i][k-1]) + ((k==j) ? 0 : dp[k+1][j]);
-                    mini = min(mini, cutss);
-                }
-                dp[i][j] = mini;
+        //             cutss = cut + ((k==i) ? 0 : dp[i][k-1]) + ((k==j) ? 0 : dp[k+1][j]);
+        //             mini = min(mini, cutss);
+        //         }
+        //         dp[i][j] = mini;
 
-            }
-        }
+        //     }
+        // }
 
-        return dp[1][m-2];
+        // return dp[1][m-2];
     }
 };
