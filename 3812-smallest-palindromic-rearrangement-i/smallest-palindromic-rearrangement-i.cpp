ef8a9af1b855786f9f9070021fa3cdfcaa28c3df
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-07-28 05:44:53 +0530

    Time: 295 ms (5.05%) | Memory: 78 MB (21.54%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        sort(s.begin(), s.end());
+
+        map<char, int> mpp;
+
+        string ans = "";
+
+        for(int i = 0; i<s.size(); i++){
+            mpp[s[i]]++;
+            // if(i%2 == 0) ans+=s[i];
+        }
+
+        string b = "";
+
+        for(auto &it: mpp){
+            if (it.second%2){
+                it.second-=1;
+                b+=it.first;
+            }
+
+            while(it.second){
+                ans+=it.first;
+                it.second-=2;
+            }
+
+            cout<<it.first<<' '<<it.second<<endl;
+        }
+
+        // cout<<endl;
+
+        cout<<ans;
+
+        string c = ans;
+        reverse(ans.begin(), ans.end());
+        // if (s.size()%2 == 0) c+=ans;
+        // else{
+        //     // c.pop_back();
+        // }
+        c = c + b + ans;
+
+        return c;
+    }
+};
