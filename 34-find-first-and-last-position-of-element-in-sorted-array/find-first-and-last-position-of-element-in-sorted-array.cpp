de9e0c689c6de8e8059fa31f42a79d74dc87024a
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-08-03 17:16:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (54.72%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -9,7 +9,7 @@ public:
 
         int last = upper_bound(nums.begin(), nums.end(), target) - nums.begin() - 1;
 
-        cout<<first<<' '<<last;
+        // cout<<first<<' '<<last;
 
         if(first<n && last<n && 0<=first && 0<=last && nums[first] == target && nums[last] == target) return {first, last};
         return {-1,-1};
