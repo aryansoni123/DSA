bbf84c28f898c4a71182a3740f9360c3d7a60900
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-08-03 20:00:52 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (80.56%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -9,14 +9,13 @@ public:
             int m = l + (h-l)/2;
 
             if(nums[l]>nums[m]){
-                h = m;
+                h = m; // pick smallest from right sorted half and discard the rest
             } else if(nums[h]<nums[m]){
                 l = m + 1;
             } else{
-                break;
+                return nums[l];
             }
         }
-
         return nums[l];
     }
 };
