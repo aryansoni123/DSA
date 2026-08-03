a548cf9d7b1a2a28893556fedf0a8f1494a50968
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-08-03 13:24:21 +0530

    Time: 19 ms (74.18%) | Memory: 136.2 MB (76.04%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -8,29 +8,32 @@ public:
 
         int pick = 0, ans = INT_MIN;
 
-        // if (turn){
-            for(int k = i; k<i+3 && k<nums.size(); k++){
-                pick += nums[k];
-                ans = max(ans, pick - f(k+1, nums, dp));
-            }
-        // }
+        for(int k = i; k<i+3 && k<nums.size(); k++){
+            pick += nums[k];
+            ans = max(ans, pick - f(k+1, nums, dp));
+        }
 
         return dp[i] = ans;
-        // else{
-        //     for(int k = i; k<i+3 && k<nums.size(); k++){
-        //         pick += nums[k];
-        //         ans = max(ans, take - f(i+k, 0, nums));
-        //     }
-        // }
     }
 
     string stoneGameIII(vector<int>& stoneValue) {
         int n = stoneValue.size();
 
-        vector<int> dp(n, -1);
+        vector<int> dp(n+1, 0);
 
-        int ans = f(0, stoneValue, dp);
+        // int ans = f(0, stoneValue, dp);
 
+        for(int i = n-1; i>=0; i--){
+            int ans = INT_MIN, pick = 0;
+            
+            for(int k = i; k<i+3 && k<n; k++){
+                pick+=stoneValue[k];
+                ans = max(ans, pick - dp[k+1]);
+            }
+            dp[i] = ans;
+        }
+
+        int ans = dp[0];
 
         if(ans>0) return "Alice";
         else if(ans<0) return "Bob";
