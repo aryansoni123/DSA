cc2342ca77efa196648b294b432251147411a39b
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-08-04 16:37:56 +0530

    Time: 2 ms (19.82%) | Memory: 8.6 MB (51.04%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        double ans = x;
+
+        int l = 1, h = x;
+        
+        while (l<=h){
+            long long m = l+(h-l)/2;
+
+            double sqr = m*m;
+
+            if(sqr>=x){
+                ans = m;
+                h = m-1;
+            } else{
+                l = m+1;
+            }
+        }
+
+        if (ans*ans == x) return (int)ans;
+        return (int)ans-1;
+    }
+};
