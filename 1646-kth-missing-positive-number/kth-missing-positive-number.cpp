ecda524b17aa8410aaf5b45869384cd5bf05f8d1
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-08-04 23:03:10 +0530

    Time: 1 ms (7.90%) | Memory: 13.2 MB (61.08%) - LeetSync

---
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n = arr.size();
+
+        if(k<arr[0]) return k;
+
+        int l = 0, h = n-1;
+
+        while(l<=h){
+            int m = l+(h-l)/2;
+
+            int miss = arr[m] - m - 1;
+
+            if(miss<k){
+                l = m+1;
+            } else{
+                h = m - 1;
+            }
+        }
+
+        cout<<h<<" "<<l<<endl;
+
+        cout<<arr[h]<<endl;
+
+        int missing = arr[h] - h - 1;
+        cout<<missing;
+        int need = k - missing;
+
+        return arr[h] + need;
+    }
+};
