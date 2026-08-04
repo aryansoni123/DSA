18fee1d08c56f8f3bdcbca94a103c465a815e408
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-08-04 15:20:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (41.44%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -8,10 +8,16 @@ public:
         if(nums[0]>nums[1]) return 0;
         if(nums[n-1]>nums[n-2]) return n-1;
 
-        for(int i = 1; i<n-1; i++){
-            if(nums[i]>nums[i+1] && nums[i]>nums[i-1]) return i;
-        }
+        int l = 1, h = n-2;
 
+        while(l<=h){
+            int m = l + (h-l)/2;
+
+            if(nums[m] > nums[m-1] && nums[m] > nums[m+1]) return m;
+            else if(nums[m]>nums[m-1]) l = m + 1;
+            else h = m-1;
+        }
+        
         return 0;
     }
 };
