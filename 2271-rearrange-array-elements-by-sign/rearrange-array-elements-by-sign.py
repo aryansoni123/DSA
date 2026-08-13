1d84499780f4d37a51af557ef1b4d52a763f6ef9
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-08-13 17:02:33 +0530

    Time: 37 ms (83.31%) | Memory: 43.9 MB (19.01%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -10,29 +10,11 @@ class Solution:
             if i>=0:
                 pos.append(i)
 
-        # f = nums[0]>=0
-
-        # print(f)
-
-        # ans = []
-
-        # if f:
-        #     ans.append(pos.pop())
 
         ans = [0]*(len(nums))
         ans[0::2] = pos
         ans[1::2] = neg
 
-        # n = len(nums)
-
-        # while n>0:
-        #     if len(pos):
-        #         ans.append(pos.pop())
-
-        #     if len(neg):
-        #         ans.append(neg.pop())
-        #     n-=2
-        
         return ans
         
             
