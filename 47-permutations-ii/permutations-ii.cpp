8b702400edc16de8e6487164afba4261cca51389
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-08-19 03:39:41 +0530

    Time: 31 ms (14.14%) | Memory: 12.7 MB (35.03%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+
+    void f(int i, vector<int> &nums, vector<int> &temp, set<vector<int>> &ans, vector<int> &freq){
+        int n = nums.size();
+
+        if(i == n){
+            ans.insert(temp);
+            return;
+        }
+
+        for(int j = 0; j<n; j++){
+            // if (j!=0 && nums[j-1] == nums[j]) continue;
+
+            if(freq[j] == 0){
+                temp.push_back(nums[j]);
+                freq[j] = 1;
+                f(i+1, nums, temp, ans, freq);
+                freq[j] = 0;
+                temp.pop_back();
+            }
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        set<vector<int>> s;
+        vector<int> temp;
+        vector<int> freq(n, 0);
+        vector<vector<int>> ans;
+        
+        f(0, nums, temp, s, freq);
+
+        cout<<s.size();
+
+        for(auto x: s){
+            ans.push_back(x);
+        }
+
+        return ans;
+    }
+};
