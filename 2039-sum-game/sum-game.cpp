d14ebf362ecbd02526b35a030d6fb29cbb364ba7
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-08-23 18:11:08 +0530

    Time: 1 ms (77.62%) | Memory: 14 MB (67.83%) - LeetSync

---
+++ 2039-sum-game/sum-game.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int q = 0, s = 0;
+        int n = num.size();
+
+        for(int i = 0; i<n; i++){
+            if(num[i] == '?'){
+                if(i+1>n/2) q++;
+                else q--;
+            } 
+            else{
+                if(i+1>n/2) s+=num[i] - '0';
+                else s-=num[i] - '0';
+            }
+        }
+
+        return (2*s != -q*9);
+
+        // if(q == 0) return s!=0;
+
+        // if(q>0){
+        //     if (abs(s)>9) return true;
+        //     else return false;
+        // } else return s!=0;
+
+        // cout<<s<<' '<<q;
+
+        // // if()
+
+        // return true;
+    }
+};
