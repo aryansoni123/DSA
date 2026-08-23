ce468ee76009f5b36d0ca4afc1b8f42e8515f6b5
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-08-24 01:47:46 +0530

    Time: 0 ms (100.00%) | Memory: 50.4 MB (5.96%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -4,18 +4,38 @@ public:
         int n = mat.size();
         int m = mat[0].size();
 
-        int mx = 0;
+        // int mx = 0;
+
+        int l = 0, h = m-1;
+
         vector<int> ind(2, 0);
 
-        for(int i = 0; i<n ; i++)
-            for(int j = 0; j<m; j++){
-                if(mat[i][j] > mx){
-                    ind = {i, j};
-                    mx = mat[i][j];
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            int mx = 0;
+            for(int j = 0; j<n; j++){
+                if(mat[j][mid] > mx){
+                    ind = {j, mid};
+                    mx = mat[j][mid];
                 }
-            } 
-            // mx = max(mx, x);
+            }
 
-        return ind;
+            cout<<l<<h<<mid<<endl;
+
+            cout<<mx<<endl;
+
+            int left = (mid-1 >= 0) ? mat[ind[0]][mid-1] : -1;
+            int right = (mid+1 < m) ? mat[ind[0]][mid+1] : -1;
+            // int right = mid+1;
+
+            if(right < mx && left<mx) return ind;
+            else if(left>mx){
+                h = mid - 1;
+            } else{
+                l = mid + 1;
+            }
+        }
+
+        return {-1, -1};
     }
 };
