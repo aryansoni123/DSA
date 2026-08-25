17327ce3e3523a6bea4ea3c7f7ef4b014f386c76
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-08-25 13:51:25 +0530

    Time: 0 ms (100.00%) | Memory: 24.5 MB (57.48%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -2,13 +2,24 @@ class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
         map<int, int> mpp;
+        vector<int> freq(101, 0);
 
-        for(auto x: nums) mpp[x]++;
+        for(auto x: nums) freq[x] = 1;
 
-        for(int i = k; i<=200; i=i+k){
-            if(mpp[i] == 0) return i;
+        int i = k;
+
+        while(i<=100){
+            if(!freq[i]) return i;
+
+            i+=k;
         }
 
-        return -1;
+        return i;
+
+        // for(int i = k; i<=200; i=i+k){
+        //     if(mpp[i] == 0) return i;
+        // }
+
+        // return -1;
     }
 };
