cb70ee116d6454c32e5df6f757761ed954a6e8dd
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-08-27 20:27:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (37.37%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(auto x: s){
+            if(x == '(' || x == '[' || x == '{') st.push(x);
+
+            else{
+                if(st.empty()) return false;
+                else if(x==')' && st.top() == '(') st.pop();
+                else if(x==']' && st.top() == '[') st.pop();
+                else if(x=='}' && st.top() == '{') st.pop();
+                else return false;
+            }
+        }
+
+        // while(!st.empty()){
+        //     cout<<st.top();
+        //     st.pop();
+        // }
+
+
+        return st.size() == 0;
+
+    }
+};
