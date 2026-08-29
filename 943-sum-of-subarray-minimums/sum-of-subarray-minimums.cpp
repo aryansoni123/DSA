3ebaa52105ca7060902b1ac428df0ddfa0de323f
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-08-30 02:16:34 +0530

    Time: 117 ms (7.54%) | Memory: 193.5 MB (35.24%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+
+        int MOD = pow(10, 9) + 7;
+
+        int n = arr.size();
+
+        vector<int> pse(n, -1);
+        vector<int> nse(n, n);
+
+        stack<int> st;
+
+        for(int i = 0; i<n; i++){
+            if(st.empty()) st.push(i);
+
+            else{
+                while(!st.empty() && arr[st.top()] > arr[i]){
+                    st.pop();
+                }
+
+                if(st.empty()) st.push(i);
+                else if(arr[st.top()] <= arr[i]){
+                    pse[i] = st.top();
+                    st.push(i);
+                }
+            }
+        }
+        
+        st = {};
+        
+        for(int i = n-1; i>=0; i--){
+            if(st.empty()) st.push(i);
+
+            else{
+                while(!st.empty() && arr[st.top()] >= arr[i]){
+                    st.pop();
+                }
+
+                if(st.empty()) st.push(i);
+                else if(arr[st.top()] <= arr[i]){
+                    nse[i] = st.top();
+                    st.push(i);
+                }
+            }
+        }
+
+        long long ans = 0;
+
+        for(int i = 0; i<n; i++){
+            long long val = ((long long)(i-pse[i]) * (long long)(nse[i]-i) * (long long)arr[i]) % (long long)MOD;
+            ans += val;
+            ans%=MOD;
+        }
+
+        return ans%MOD;
+    }
+};
