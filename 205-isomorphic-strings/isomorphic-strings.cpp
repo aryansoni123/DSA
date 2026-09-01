f8d628ff199259de5a7295c20c7d9b7a1192746a
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-09-01 22:19:06 +0530

    Time: 23 ms (6.44%) | Memory: 9.1 MB (98.87%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -7,24 +7,22 @@ public:
         int n = s.size();
 
         for(int i= 0; i<n; i++){
-            for(auto &p: mpp){
-                if(p.second == t[i] && p.first!=s[i]) return false;
-            }
+            // for(auto &p: mpp){
+            //     if(p.second == t[i] && p.first!=s[i]) return false;
+            // }
 
             if(!mpp.contains(s[i])){
                 mpp[s[i]] = t[i];
+                for(auto &p: mpp){
+                    if(p.second == t[i] && p.first!=s[i]) return false;
+                }
             } else{
                 if(mpp[s[i]] != t[i]) return false;
+                for(auto &p: mpp){
+                    if(p.second == t[i] && p.first!=s[i]) return false;
+                }
             }
-
-            // cout<<s[i]<<' '<<mpp[s[i]];
-            // cout<<endl;
         }
-
-        // for(auto x: mpp){
-        //     cout<<x.first<<' '<<x.second;
-        //     cout<<endl;
-        // }
 
         return true;
     }
