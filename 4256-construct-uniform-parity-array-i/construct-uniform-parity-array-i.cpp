96f94b3d7450c9a6140b4e5319fbbcad04bf37fa
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-09-02 14:31:42 +0530

    Time: 0 ms (100.00%) | Memory: 30.1 MB (94.95%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -1,48 +1,50 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int n = nums1.size();
+        // int n = nums1.size();
 
-        bool ans = true;
+        // bool ans = true;
 
-        // bool it = true;
+        // // bool it = true;
 
-        for(int i = 0; i<n; i++){
-            bool it = true;
+        // for(int i = 0; i<n; i++){
+        //     bool it = true;
 
-            if(nums1[i]%2) continue;
+        //     if(nums1[i]%2) continue;
 
-            for(int j = 0; j<n; j++){
-                bool it = false;
+        //     for(int j = 0; j<n; j++){
+        //         bool it = false;
 
-                if(i!= j && (nums1[i] - nums1[j])%2){
-                    break;
-                }
-            }
+        //         if(i!= j && (nums1[i] - nums1[j])%2){
+        //             break;
+        //         }
+        //     }
 
-            ans = ans&it;
-        }
+        //     ans = ans&it;
+        // }
 
-        if(ans) return ans;
+        // if(ans) return ans;
 
-        ans = true;
+        // ans = true;
 
-        for(int i = 0; i<n; i++){
-            bool it = true;
+        // for(int i = 0; i<n; i++){
+        //     bool it = true;
 
-            if(nums1[i]%2 == 0) continue;
+        //     if(nums1[i]%2 == 0) continue;
 
-            for(int j = 0; j<n; j++){
-                bool it = false;
+        //     for(int j = 0; j<n; j++){
+        //         bool it = false;
 
-                if(i!= j && (nums1[i] - nums1[j])%2 == 0){
-                    break;
-                }
-            }
+        //         if(i!= j && (nums1[i] - nums1[j])%2 == 0){
+        //             break;
+        //         }
+        //     }
 
-            ans = ans&it;
-        }
+        //     ans = ans&it;
+        // }
 
-        return ans;
+        // return ans;
+
+        return true;
     }
 };
