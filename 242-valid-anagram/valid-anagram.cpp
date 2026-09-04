90b00f3ce739662802be9b4658a1f55ae32ed6fd
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-09-04 12:38:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (25.79%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int> sf(26, 0);
-        vector<int> tf(26, 0);
+        vector<int> freq(26, 0);
 
-        for(auto x: s) sf[x - 'a']++;
-        for(auto x: t) tf[x - 'a']++;
+        // for(auto x: s) sf[x - 'a']++;
+        // for(auto x: t) tf[x - 'a']++;
 
+        if(s.size()!=t.size()) return false;
+
+        for(int i = 0; i<s.size(); i++){
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
         for(int i = 0; i<26; i++){
-            if(sf[i] != tf[i]) return false;
+            if(freq[i]) return false;
         }
 
         return true;
