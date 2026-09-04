70cabf8e70b13734f9bb52a1e4afb302a47509fd
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-09-04 11:44:59 +0530

    Time: 8 ms (70.20%) | Memory: 210.9 MB (15.33%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -11,17 +11,8 @@ public:
 
         for(int i = 0; i<n; i++){
             pref[i+1] = max(pref[i], nums[i]);
+            suff[n-i-1] = min(suff[n-i], nums[n-i-1]);
         }
-
-        for(int i = n; i>0; i--){
-            suff[i-1] = min(suff[i], nums[i-1]);
-        }
-
-        // for(auto x: pref) cout<<x<<' ';
-        // cout<<endl;
-        // for(auto x: suff) cout<<x<<' ';
-
-        suff.pop_back();
 
         for(int i = 0; i<n; i++){
             if(pref[i+1]-suff[i] <= k) return i;
