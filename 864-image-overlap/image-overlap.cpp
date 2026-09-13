96e22fcb9ea72a11ade4eb32dc4550fee032ed44
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-09-14 01:16:25 +0530

    Time: 43 ms (77.41%) | Memory: 12.9 MB (86.13%) - LeetSync

--- 864-image-overlap/image-overlap.cpp
+++ 864-image-overlap/image-overlap.cpp
@@ -8,9 +8,7 @@ public:
         for(int i = -(n-1); i<n; i++){
             for(int j = -(n-1); j<n; j++){
                 int cnt = 0;
-                // int cnt2 = 0;
-                // int cnt3 = 0;
-                // int cnt4 = 0;
+                
                 for(int r = 0; r<n; r++){
                     int nr = (i+r);
                     if(nr>=n || nr<0) continue;
@@ -19,14 +17,9 @@ public:
                         int nc = (j+c);
 
                         if(nc>=n || nc<0) continue;
-                        if(img1[nr][nc] & img2[r][c]) cnt++;  //DR
-                        // if(img2[nr][nc] & img1[r][c]) cnt2++; //UL
-                        // if(img1[nr][c] & img2[r][nc]) cnt3++; //DL
-                        // if(img2[r][nc] & img1[nr][c]) cnt4++; //UR
+                        if(img1[nr][nc] & img2[r][c]) cnt++;  
                     }
                 }
-
-                // int mx = max(max(cnt, cnt2), max(cnt3, cnt4));
 
                 ans = max(cnt, ans);
             }
