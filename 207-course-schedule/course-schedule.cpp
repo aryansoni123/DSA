9f9f131a76b9b2649e5164686087ea003f6a2618
Arin Soni <64584332+aryansoni123@users.noreply.github.com>
2026-09-24 20:21:53 +0530

    Time: 11 ms (21.98%) | Memory: 21 MB (7.43%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+
+        vector<int> indeg(n, 0);
+        // vector<vector<int>>(adj)
+        unordered_map<int, vector<int>> adj;
+
+        for(auto x: prerequisites){
+            indeg[x[1]]++;
+            adj[x[0]].push_back(x[1]);
+        }
+
+        queue<int> q;
+        vector<int> vis(n, 0);
+
+        for(int i = 0; i<n; i++){
+            if(indeg[i] == 0) q.push(i);
+        }
+
+        int cnt = 0;
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+
+            vis[node] = 1;
+
+            cnt++;
+
+            for(auto x: adj[node]){
+                indeg[x]--;
+                if(!indeg[x] && !vis[x]) q.push(x);
+            }
+        }
+
+        return cnt == n;
+    }
+};
